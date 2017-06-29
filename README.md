f1e8d8f9c28b289f2a0e5eab8a306a40cf6e3cdb
Tom Hodgins <tomhodgins@gmail.com>
2017-06-28 20:27:27 -0400

    🤗 Initial commit 🎉

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # reprocss
-A flexible &lt;style>-tag based CSS reprocessor
+
+**A flexible CSS reprocessor using <style> tags**
+
+Can you imagine if you could interpolate JS inside CSS with the `${}` syntax, and also control when and how frequently that CSS reprocessed with a process="" attribute on the `<style>` tag:
+
+![](http://i.imgur.com/6SkRCIm.png)
+
+```html
+<style process="none"></style>
+<style process="once"></style>
+<style process="auto"></style>
+<style process="touchstart mousedown"></style>
+```
+
+## How to use reproCSS
+
+Include the `reprocss.js` JavaScript plugin in your HTML:
+
+```html
+<script src="reprocss.js"></script>
+```
+
+The plugin is also a UMD module if you want to use the plugin inside JS modules.
+
+## Examples
+
+### Interpolating JS-supplied values in CSS content:;
+
+![](http://i.imgur.com/cW0yNMG.png)
+
+```html
+<div>Hello</div>
+
+<script>
+  var user = 'Username'
+</script>
+
+<style process="once">
+  div:after {
+    content: ' ${user}';
+  }
+</style>
+```
+
+### Element Queries via a JS Selector Resolver
+
+![](http://i.imgur.com/1mGaC41.png)
+
+```html
+<div id="demo">
+  <p>Hello</p>
+</div>
+
+<style process="resize">
+  ${demo.offsetWidth > 400 && "#demo"} {
+    background: lime;
+  }
+  ${demo.offsetWidth > 400 && "#demo"} p {
+    color: red;
+  }
+</style>
+```
+
+### JS interpolation in CSS
+
+![](http://i.imgur.com/rNZHUXN.png)
+
+```html
+<textarea id="demo"></textarea>
+
+<style process="input">
+  #demo {
+    background: hsl(${demo.value.length}, 50%, 50%)
+  }
+</style>
+```
+
+> Made with ♥ by [@innovati](http://twitter.com/innovati)
---
+++ reprocss.js
@@ -0,0 +1,191 @@
+
+/*
+
+  # reproCSS
+  ## version 0.0.1
+
+  reproCSS is a CSS reprocessor that uses `<style>` tags and a `process=""` attribute.
+
+  You can add the CSS you want reprocss.js to apply to your HTML in `<style>` tags with the following values on the `process` attribute:
+
+  - `none` means no reprocessing
+  - `once` means process immediately and never again
+  - `auto` runs every `resize`, `input`, and `click` event on window
+  - any space-separated list of JS events you wish to listen for
+
+      <style process=none></style>
+      <style process=once></style>
+      <style process=auto></style>
+      <style process="touchstart scroll"></style>
+
+  To evaluate JavaScript inside the CSS as it's being reprocessed by reproCSS you can use the `${}` interpolation syntax. The following `<style>` tag would always ensure the `<div>` in this example was half of the window's height:
+
+      <style process=auto>
+        div {
+          height: calc(${innerHeight}px / 2);
+        }
+      </style>
+
+  When the browser is 1000px tall the `${innerHeight}` in our CSS will be output as `500`, leading to the following output:
+
+      <style process=auto>
+        div {
+          height: calc(500px / 2);
+        }
+      </style>
+
+  Currently this plugin only supports `<style>` tags, but it may be possible to support CSS loaded via `<link>` with a similar technique.
+
+  Author: Tommy Hodgins
+  License: MIT
+
+*/
+
+// Uses Node, AMD or browser globals to create a module
+(function (root, factory) {
+
+  if (typeof define === 'function' && define.amd) {
+
+    // AMD: Register as an anonymous module
+    define([], factory)
+
+  } else if (typeof module === 'object' && module.exports) {
+
+    // Node: Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node
+    module.exports = factory()
+
+  } else {
+
+    // Browser globals (root is window)
+    root.cursory = factory()
+
+  }
+
+}(this, function(e) {
+
+  const reprocss = {}
+
+  reprocss.count = 0
+
+  reprocss.style = []
+
+  reprocss.load = () => {
+
+    // Find all <style process=""> tags in document
+    Array.from(document.querySelectorAll('style[process]'), tag => {
+
+      // Mark tag with data-reprocss="" attribute and current tag count
+      tag.setAttribute('data-reprocss', reprocss.count)
+
+      // Increment tag count
+      reprocss.count++
+
+      reprocss.process(tag)
+
+    })
+
+  }
+
+  reprocss.process = tag => {
+
+    // Save original CSS code in the reprocss.style array
+    reprocss.style.push(tag.innerHTML)
+
+    // Remove current tag contents
+    tag.innerHTML = null
+
+    let trigger = tag.getAttribute('process')
+
+    switch (trigger) {
+
+      // process="none"
+      case "none":
+
+        // Populate tag with original CSS code
+        tag.innerHTML = reprocss.style[tag.getAttribute('data-reprocss')]
+
+        break;
+
+      // process="once"
+      case "once":
+
+        // Populate tag immediately with evaluated CSS code
+        reprocss.apply(tag.getAttribute('data-reprocss'))
+
+        break;
+
+      // process="auto"
+      case "auto":
+
+        // Populate tag immediately with evaluated CSS code
+        reprocss.apply(tag.getAttribute('data-reprocss'))
+
+        // Listen on window resize
+        window.addEventListener('resize', ()=> {
+          reprocss.apply(tag.getAttribute('data-reprocss'))
+        })
+
+        // Listen on window input
+        window.addEventListener('input', ()=> {
+          reprocss.apply(tag.getAttribute('data-reprocss'))
+        })
+
+        // Listen on window click
+        window.addEventListener('click', ()=> {
+          reprocss.apply(tag.getAttribute('data-reprocss'))
+        })
+
+        break;
+
+      // For any other process="" values
+      default:
+
+        // Populate tag immediately with evaluated CSS code
+        reprocss.apply(tag.getAttribute('data-reprocss'))
+
+        // Turn space-separated lists into an array
+        let triggers = trigger.split(' ')
+
+        // For each custom event we find
+        Array.from(triggers, event => {
+
+          // Add a new event listener to window for that event
+          window.addEventListener(event, ()=> {
+            reprocss.apply(tag.getAttribute('data-reprocss'))
+          })
+
+        })
+
+        break;
+
+    }
+
+  }
+
+  reprocss.apply = number => {
+
+    // Locate the corresponding `<style>` tag in DOM
+    let tag = document.querySelectorAll('style[data-reprocss]')[number]
+
+    // Evaluate CSS code for this tag
+    let css = reprocss.style[number].replace(/\$\{([^\}]*)\}/g, (string, match) => {
+
+      let func = new Function('return '+match)
+
+      return func()
+
+    })
+
+    // Populate `<style>` tag with evaluated CSS
+    tag.innerHTML = css
+
+  }
+
+  // Start reprocss when document is loaded
+  document.addEventListener('DOMContentLoaded', reprocss.load())
+
+  return reprocss
+
+}))
