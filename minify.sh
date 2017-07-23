8caad2e024d2213043f328a7f7b31db6fe863527
Tom Hodgins <tomhodgins@gmail.com>
2017-07-22 22:04:53 -0400

    Add minified files and minify.sh script

---
+++ minify.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Remove old files
+mkdir ./min
+rm ./min/* -v;
+
+# Minify reproCSS
+echo "Minifying reproCSS...";
+echo "/* reproCSS / Tommy Hodgins / MIT License / version 0.0.7 */" > ./min/reprocss.min.js;
+uglifyjs reprocss.js >> ./min/reprocss.min.js;
+
+# Minify Mixins
+echo "Minifying mixins...";
+cd mixins;
+for f in *.js;
+do
+  echo "/* ${f} / Tommy Hodgins / MIT License / version 0.0.7 */" > ../min/"${f%}.min.js";
+  uglifyjs ./"$f" >> ../min/"${f%}.min.js";
+done
