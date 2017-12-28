ee0b5ff4ed18534b494024c1ef4a31e5c7a44331
Tommy Hodgins <tomhodgins@gmail.com>
2017-12-28 09:51:32 -0500

    Update auto-expand

--- mixins/auto-expand.js
+++ mixins/auto-expand.js
@@ -32,17 +32,40 @@ function autoExpand(selector, direction) {
 
     if (direction == 'width' || direction == 'both') {
 
+      var borderLeft = measure(tag[i], 'border-left-width')
+      var borderRight = measure(tag[i], 'border-right-width')
+      var paddingLeft = measure(tag[i], 'padding-left')
+      var paddingRight = measure(tag[i], 'padding-right')
+
       tag[i].style.width = ''
-      tag[i].style.width = tag[i].scrollWidth + 'px'
+      tag[i].style.width = borderLeft + paddingLeft
+                           + tag[i].scrollWidth
+                           + paddingRight + borderRight + 'px'
 
     }
 
     if (direction == 'height' || direction == 'both') {
 
+      var borderTop = measure(tag[i], 'border-top-width')
+      var borderBottom = measure(tag[i], 'border-bottom-width')
+      var paddingTop = measure(tag[i], 'padding-top')
+      var paddingBottom = measure(tag[i], 'padding-bottom')
+
       tag[i].style.height = ''
-      tag[i].style.height = tag[i].scrollHeight + 'px'
+      tag[i].style.height = borderTop + paddingBottom
+                           + tag[i].scrollHeight
+                           + paddingTop + borderBottom + 'px'
 
     }
+
+  }
+
+  function measure(tag, property) {
+
+    return parseInt(
+             window.getComputedStyle(tag, null)
+               .getPropertyValue(property)
+                 .replace(/px$/, ''))
 
   }
 
